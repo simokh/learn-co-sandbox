245d0d83bdb6287050c15c8400340cc11710d517
Simo Khouili <m.simokhouili@gmail.com>
2021-02-23 16:21:30 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,48 @@
+# number = 5 
+# number.times do 
+#   puts "I print that #{number}"
+# end 
+
+# count = 0 
+# array = [1, 2 ,3, 4, 5]
+# while array [count] do 
+#   puts array [count]
+#   count += 1 
+# end 
+
+# while true do 
+#   puts "hello"
+# end 
+
+# while true {
+#   puts 'hello'
+# }
+
+# 5.times do |index|
+#   puts index 
+# end 
+
+3.times { |hot_dogs|
+  puts hot_dogs
+}
+
+
+array = [1, 2, 3, 4, 5,]
+length = array.length
+
+length.times { |index|
+  puts array [index]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+better than copy_paste!
+puts "hello World"
+# Don't do this 
+puts (1 #+1)
+# this is to calculate whatever comes after
+# print this line and then some 
+#learn faster and focus
+#hey keep on learning 
+
